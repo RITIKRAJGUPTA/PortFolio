60c0df0ff0fcf9978adc55b975b3c6441e5fc067
ritik.raj <raj.ritik2009@gmail.com>
2024-06-13 03:59:38 +0530

    done git config --list

---
+++ src/Components/About.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Ritik, a passionate Web developer with a keen eye for MERN
+          Stack .I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [MCA] [Chandigarh University] [2024], [BCA]
+          [Himachal Pradesh University] [2022], [12th]
+          [Shivalik Science School] [2018], [10th] [B.V.N School]
+          [2016]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [C++, Java, Python] Experienced with [Networking/Microcontroller] Strong grasp of [Design Principles/Concepts]
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Training
+        </h1>
+        <span>
+          [Web Developer], [Oceana technologies], [6 Months internship offer letter] 
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Certification
+        </h1>
+        <span>
+          [REACT JS SPRINGBOARD INFOSYS], [NODE JS SPRINGBOARD INFOSYS], [ADVANCED BUSINESS ANALYTICS
+            SPECILIZATION]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
